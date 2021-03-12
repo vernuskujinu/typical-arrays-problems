e1974fbcf700913b0952bca9a15887479c3f7a91
vernuskujinu <78141473+vernuskujinu@users.noreply.github.com>
2021-03-12 14:45:21 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
+exports.min = function min(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    let inf = Infinity;
+    array.forEach(item => {
+        if (item < inf) {
+            inf = item;
+        }
+    });
+    return inf;
+};
+exports.max = function max(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    let inf = -Infinity;
+    array.forEach(item => {
+        if (item > inf) {
+            inf = item;
+        }
+    });
+    return inf;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    let abc = 0;
+    array.forEach(item => {
+        abc = abc + item;
+    });
+    return abc / array.length;
+};
